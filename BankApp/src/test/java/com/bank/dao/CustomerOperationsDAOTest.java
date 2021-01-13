939f71fa6162f33dab0839e94d970440ba0e78eb
Sagar Patel <sagarpatel.sp2611@gmail.com>
2021-01-12 22:48:37 -0500

    commit on 1-12-2021 10:48 PM EST.

--- BankApp/src/test/java/com/bank/dao/CustomerOperationsDAOTest.java
+++ BankApp/src/test/java/com/bank/dao/CustomerOperationsDAOTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -15,12 +14,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
 import com.bank.dao.impl.CustomerOperationsDAOImpl;
